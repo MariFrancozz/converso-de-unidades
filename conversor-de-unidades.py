1b7badbb2057e92f3d791dff9951c566eca4f01b
Mariana Laura Franco Raymundo <marianalaurafrancoraymundo@gmail.com>
2023-09-07 20:31:29 -0300

    primeira versão do conversor de unidades
    Conversor de unidades feito em python com o intuito de praticar condicionais e funções.

---
+++ conversor-de-unidades.py
@@ -0,0 +1,15 @@
+print("\n**********Conversor Simples de Unidades**********")
+def kelforcel (n1):
+    resul = n1 - 263
+def kelforfah (n1):
+    resul = (n1 - 273) * 1.8 + 32
+def celforkel (n1):
+    resul = n1 + 273
+def celforfah (n1):
+    result = n1 * 1.8 + 32
+def fahforcel (n1):
+    result = (n1 - 32) / 1.8
+def fahforkel (n1):
+    resul = (n1 - 32) * 5 / 9 + 273
+
+
