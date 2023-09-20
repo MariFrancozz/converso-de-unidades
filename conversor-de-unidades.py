e616e6ec7b3de12780c4562692114a790f70c406
Mariana Laura Franco Raymundo <marianalaurafrancoraymundo@gmail.com>
2023-09-20 01:37:40 -0300

    prevenção de erros
    Tentei buscar soluções para possíveis erros que possam surgir durante a conversão de unidades. Faltam mais alguns ajustes.

--- conversor-de-unidades.py
+++ conversor-de-unidades.py
@@ -1,4 +1,6 @@
 print("\n**********Conversor Simples de Unidades**********")
+
+#Definição de funções
 def kelforcel (n1):
     resul = n1 - 263
     print("O resultado da conversão de kelvin para celsius é: ", resul)
@@ -18,33 +20,53 @@ def fahforkel (n1):
     resul = (n1 - 32) * 5 / 9 + 273
     print("O resultado da conversão de fahrenheit para kelvin é: ", resul)
 
-print("\n_____Conversão de Unidades de Temperatura_____")
-print("              1-kelvin para celsius")
-print("              2-kelvin para fahrenheit")
-print("              3-celsius para kelvin")
-print("              4-celsius para fahrenheit")
-print("              5-fahrenheit para celsius")
-print("              6-fahrenheit para kelvin")
+#Tabela com as conversões possíveis
+def conversoes(y):
+    print("\n_____Conversão de Unidades de Temperatura_____")
+    print("              1-kelvin para celsius")
+    print("              2-kelvin para fahrenheit")
+    print("              3-celsius para kelvin")
+    print("              4-celsius para fahrenheit")
+    print("              5-fahrenheit para celsius")
+    print("              6-fahrenheit para kelvin")
 
-convuni = input("\nDigite o número da conversão desejada: ")
-n1 = float(input("Digite o valor a ser convertido: "))
-if "1" in convuni:
+#Realizando a conversão
+convuni = int(input("\nDigite o número da conversão desejada: "))
+n1 = float(input("Digite o valor a ser convertido:  "))
+if convuni == 1:
     print(kelforcel(n1))
-elif "2" in convuni:
+elif convuni == 2:
     print(kelforfah(n1))
-elif "3" in convuni:
+elif convuni == 3:
     print(celforkel(n1))
-elif "4" in convuni:
+elif convuni == 4:
     print(celforfah(n1))
-elif "5" in convuni:
+elif convuni == 5:
     print(fahforcel(n1))
-elif "6" in convuni:
+elif convuni == 6:
     print(fahforkel(n1))
 
-
-
-
-    
-
-
-
+#Tentativa de prevenção de erro caso a pessoa não selecione algumas das conversões possíveis 
+else:
+    print("Você não selecionou uma das opções válidas.")
+    escolha = input("Digite y para tentar novamente ou n para sair do conversor: ")
+    if escolha == "y":
+        print(conversoes(2))
+        convuni = int(input("\nDigite o número da conversão desejada: "))
+        n1 = float(input("Digite o valor a ser convertido:  "))
+        if convuni == 1:
+            print(kelforcel(n1))
+        elif convuni == 2:
+            print(kelforfah(n1))
+        elif convuni == 3:
+            print(celforkel(n1))
+        elif convuni == 4:
+            print(celforfah(n1))
+        elif convuni == 5:
+            print(fahforcel(n1))
+        elif convuni == 6:
+            print(fahforkel(n1))
+        else:
+            print("Você saiu do conversor")
+                
+          
